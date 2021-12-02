75965478a62d47d1177df35f76773cf5f90d8211
Fedex159 <fede.gallina94@gmail.com>
2021-12-02 16:58:18 -0300

    startscreen created and structured

---
+++ src/components/StartScreen/StartScreen.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import profile from "../../assets/imgs/profile.jpg";
+import s from "./StartScreen.module.css";
+
+function StartScreen() {
+  return (
+    <div className={s.container}>
+      <div className={s.info}>
+        <h4>Hi There 👋 I'm</h4>
+        <p>Federico{"\n"}Avelin</p>
+        <h3>FullStack Web Developer</h3>
+        <div>A fullstack developer from Argentina, I ❤️ React and Express.</div>
+        <div className={s.button}>Hire Me</div>
+      </div>
+      <img src={profile} alt="profile" />
+    </div>
+  );
+}
+
+export default StartScreen;
