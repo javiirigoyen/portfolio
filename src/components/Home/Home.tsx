e3789cdef20501b935d114d5b5e9ef36bca3a723
Fedex159 <fede.gallina94@gmail.com>
2021-12-02 16:34:10 -0300

    load startscreen home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
+import StartScreen from "../StartScreen/StartScreen";
 
 function Home() {
   return (
     <div>
       <NavBar />
+      <StartScreen />
     </div>
   );
 }
