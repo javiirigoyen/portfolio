667d78a7ba950b703447713aaa4a32ec5df1ed85
Fedex159 <fede.gallina94@gmail.com>
2021-12-02 18:23:50 -0300

    load about home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
 import StartScreen from "../StartScreen/StartScreen";
+import About from "../About/About";
 import s from "./Home.module.css";
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
     <div className={s.container}>
       <NavBar />
       <StartScreen />
+      <About />
     </div>
   );
 }
