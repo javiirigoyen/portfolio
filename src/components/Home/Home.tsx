c0a1c5bc2bb6f8eb678e118c1f98f6c299e07229
Fedex159 <fede.gallina94@gmail.com>
2021-12-02 19:26:42 -0300

    load technologies home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "../NavBar/NavBar";
 import StartScreen from "../StartScreen/StartScreen";
 import About from "../About/About";
+import Technologies from "../Technologies/Technologies";
 import s from "./Home.module.css";
 
 function Home() {
@@ -10,6 +11,7 @@ function Home() {
       <NavBar />
       <StartScreen />
       <About />
+      <Technologies />
     </div>
   );
 }
