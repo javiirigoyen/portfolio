21baab4beb17c5983279d2e7bb3e531b4e385c23
Fedex159 <fede.gallina94@gmail.com>
2021-12-02 15:39:01 -0300

    load navbar in home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import NavBar from "../NavBar/NavBar";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 
 export default Home;
