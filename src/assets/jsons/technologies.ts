6eaef71410325aa4fba80dc0d2e947044c3de55f
Fedex159 <fede.gallina94@gmail.com>
2021-12-02 19:26:33 -0300

    add technologies json

---
+++ src/assets/jsons/technologies.ts
@@ -0,0 +1,67 @@
+interface Tec {
+  title: string;
+  img: string;
+}
+
+export const technologies: Tec[] = [
+  {
+    title: "HTML5",
+    img: "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
+  },
+  {
+    title: "CSS",
+    img: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+  },
+  {
+    title: "Javascript",
+    img: "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg",
+  },
+  {
+    title: "React",
+    img: "https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg",
+  },
+  {
+    title: "Redux",
+    img: "https://raw.githubusercontent.com/sachinverma53121/sachinverma53121/master/icons/redux.png",
+  },
+  {
+    title: "Node",
+    img: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg",
+  },
+  {
+    title: "Express",
+    img: "https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg",
+  },
+  {
+    title: "Postgresql",
+    img: "https://raw.githubusercontent.com/sachinverma53121/sachinverma53121/master/icons/psql.png",
+  },
+  {
+    title: "Git",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png",
+  },
+  {
+    title: "Npm",
+    img: "https://upload.wikimedia.org/wikipedia/commons/d/db/Npm-logo.svg",
+  },
+  {
+    title: "Material UI",
+    img: "https://cdn.worldvectorlogo.com/logos/material-ui-1.svg",
+  },
+  {
+    title: "Linux",
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg",
+  },
+  {
+    title: "Postman",
+    img: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+  },
+  {
+    title: "Heroku",
+    img: "https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg",
+  },
+  {
+    title: "Vercel",
+    img: "https://camo.githubusercontent.com/add2c9721e333f0043ac938f3dadbc26a282776e01b95b308fcaba5afaf74ae3/68747470733a2f2f6173736574732e76657263656c2e636f6d2f696d6167652f75706c6f61642f76313538383830353835382f7265706f7369746f726965732f76657263656c2f6c6f676f2e706e67",
+  },
+];
