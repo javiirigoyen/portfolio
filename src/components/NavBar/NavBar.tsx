69e4213d5e3413b735deeecad8e836c0e23e9f87
Fedex159 <fede.gallina94@gmail.com>
2021-12-02 16:01:42 -0300

    add css and structure navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import logo from "../../assets/imgs/logo.png";
+import s from "./NavBar.module.css";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div className={s.nav}>
+      <div className={s.container}>
+        <img src={logo} alt="logo" />
+        <div className={s.buttons}>
+          <div>Home</div>
+          <div>About</div>
+          <div>Tecnologies</div>
+          <div>Projects</div>
+          <div>Contact Me</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
