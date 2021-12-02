017fdef888f6a816258412b1a8d39a874b0c9594
Fedex159 <fede.gallina94@gmail.com>
2021-12-02 16:57:21 -0300

    add background color home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
 import StartScreen from "../StartScreen/StartScreen";
+import s from "./Home.module.css";
 
 function Home() {
   return (
-    <div>
+    <div className={s.container}>
       <NavBar />
       <StartScreen />
     </div>
