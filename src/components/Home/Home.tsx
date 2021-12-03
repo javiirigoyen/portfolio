c350fa6b0a81a95ed716305fee1983b20146e83d
Fedex159 <fede.gallina94@gmail.com>
2021-12-03 10:31:07 -0300

    load project in home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,6 +3,7 @@ import NavBar from "../NavBar/NavBar";
 import StartScreen from "../StartScreen/StartScreen";
 import About from "../About/About";
 import Technologies from "../Technologies/Technologies";
+import Projects from "../Projects/Projects";
 import s from "./Home.module.css";
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
       <StartScreen />
       <About />
       <Technologies />
+      <Projects />
     </div>
   );
 }
