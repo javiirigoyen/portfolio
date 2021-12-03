6f199deba0dae60bd483bde00163d6428bd935ef
Fedex159 <fede.gallina94@gmail.com>
2021-12-03 10:31:36 -0300

    project componente created and structured

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { projects } from "../../assets/jsons/projects";
+import Card from "./Card/Card";
+import s from "./Projects.module.css";
+
+function Projects() {
+  return (
+    <div className={s.projects}>
+      <div className={s.container}>
+        <h2>Projects</h2>
+        <div className={s.items}>
+          {projects.map((p, i) => (
+            <Card
+              key={`${p.title}_${i}`}
+              title={p.title}
+              img={p.img}
+              description={p.description}
+              prod={p.prod}
+              repo={p.repo}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
