1097bf6c4197f74b532481964830fcbaca8333df
Fedex159 <fede.gallina94@gmail.com>
2021-12-03 17:31:26 -0300

    load dots in technologies

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { technologies } from "../../assets/jsons/technologies";
+import Dots from "../Dots/Dots";
 import s from "./Technologies.module.css";
 
 function Technologies() {
   return (
     <div id="technologies" className={s.technologies}>
+      <Dots top="50%" left="88%" />
       <div className={s.container}>
         <h2>Technologies</h2>
         <div className={s.items}>
