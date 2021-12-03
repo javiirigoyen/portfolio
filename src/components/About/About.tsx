b6410880087e358ce6c4c794c18885d051712ea9
Fedex159 <fede.gallina94@gmail.com>
2021-12-03 17:03:40 -0300

    add function to button

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import about from "../../assets/imgs/about2.jpg";
 import s from "./About.module.css";
 
+const link: string =
+  "https://drive.google.com/file/d/1SbpEuBhXu_17X85YFGg7HFuSl8UqvuGB/view?usp=sharing";
+
 function About() {
   return (
     <div id="about" className={s.about}>
@@ -13,7 +16,9 @@ function About() {
         <div className={s.info}>
           <h2>About Me</h2>
           <p>{`I'm Federico Avelin, FullStack developer. I am passionate about leveraging my diverse backgrounds to decipher challenging problems and create enjoyable experiences. I perfect my skills every day, to keep up to date and continue advancing in my career as a developer.\n\nI develop websites with HTML, CSS and JavaScript. For the front, I love to use React alongside Redux, and for the back Express along with PostgresSQL and Sequelize.\n\nI always work to achieve the best result in each project that I have in my hands, with me you will find someone who will not give up easily`}</p>
-          <div className={s.button}>Download CV</div>
+          <a href={link} rel="noopener noreferrer" target="_blank">
+            <div className={s.button}>Download CV</div>
+          </a>
         </div>
       </div>
       ;
