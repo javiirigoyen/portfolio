b71306d61dcde0c1552c419814fbe1d200897029
Fedex159 <fede.gallina94@gmail.com>
2021-12-03 17:28:38 -0300

    created and structured dots component

---
+++ src/components/Dots/Dots.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import dots from "../../assets/imgs/dots.png";
+import s from "./Dots.module.css";
+
+interface DotsProps {
+  top: string;
+  left: string;
+}
+
+function Dots({ top, left }: DotsProps) {
+  return (
+    <div className={s.container}>
+      <img src={dots} alt="dots" style={{ top: top, left: left }} />
+    </div>
+  );
+}
+
+export default Dots;
