a0aa5acf3d26037e9a5e5f9daa97a2ef63d2ea81
Fedex159 <fede.gallina94@gmail.com>
2021-12-03 11:29:18 -0300

    info created and structured

---
+++ src/components/Contact/Info/Info.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { InfoObj, info } from "../../../assets/jsons/info";
+import s from "./Info.module.css";
+
+function ButtonInfo({ src, alt, title, href }: InfoObj) {
+  return (
+    <div className={s.button}>
+      <img src={src} alt={alt} />
+      <a href={href} rel="noopener noreferrer" target="_blank">
+        <h4>{title}</h4>
+      </a>
+    </div>
+  );
+}
+
+function Info() {
+  return (
+    <div className={s.container}>
+      {info.map((i) => (
+        <ButtonInfo src={i.src} alt={i.alt} title={i.title} href={i.href} />
+      ))}
+    </div>
+  );
+}
+
+export default Info;
