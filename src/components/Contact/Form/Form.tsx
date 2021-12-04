852252f5d1bfa7be466c4085cffcbce00a1bb58c
Fedex159 <fede.gallina94@gmail.com>
2021-12-04 20:33:18 -0300

    config emailjs

--- src/components/Contact/Form/Form.tsx
+++ src/components/Contact/Form/Form.tsx
@@ -1,14 +1,56 @@
 import React from "react";
+import emailjs from "emailjs-com";
 import s from "./Form.module.css";
 
 function Form() {
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    // response to me
+    emailjs
+      .sendForm(
+        "gmail",
+        "template_59ui8wh",
+        e.target,
+        "user_nFqzK1oa18vqJa7uMu3N6"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    // response to user
+    emailjs
+      .sendForm(
+        "gmail",
+        "template_v487cat",
+        e.target,
+        "user_nFqzK1oa18vqJa7uMu3N6"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+    alert("Message send");
+  };
+
   return (
     <div className={s.container}>
-      <form>
-        <input placeholder="Full Name*" required />
-        <input placeholder="Email*" required />
-        <input placeholder="Subject*" required />
-        <textarea rows={5} placeholder="Message*" required />
+      <form onSubmit={sendEmail}>
+        <input type="text" placeholder="Full Name*" required name="name" />
+        <input type="text" placeholder="Email*" required name="email" />
+        <input type="text" placeholder="Subject*" required name="subject" />
+        <textarea rows={5} placeholder="Message*" required name="message" />
         <button type="submit">Submit</button>
       </form>
     </div>
