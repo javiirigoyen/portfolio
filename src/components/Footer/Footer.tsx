6789d53e4db153372a4a7359518010d4343bdf53
Fedex159 <fede.gallina94@gmail.com>
2021-12-04 20:52:19 -0300

    footer created

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from "./Footer.module.css";
+
+function Footer() {
+  return <div className={s.container}>Federico Avelin ® 2021</div>;
+}
+
+export default Footer;
