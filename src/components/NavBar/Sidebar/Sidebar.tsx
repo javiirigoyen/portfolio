fcac56e12684e62b2fe47b1b593975880f233d39
Fedex159 <fede.gallina94@gmail.com>
2021-12-05 11:49:06 -0300

    created sidebar component

---
+++ src/components/NavBar/Sidebar/Sidebar.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import hamburger from "../../../assets/imgs/hamburger.png";
+import { items, NavItem } from "../NavBar";
+import s from "./Sidebar.module.css";
+
+interface SidebarProps {
+  select: string;
+}
+
+function Sidebar({ select }: SidebarProps) {
+  const [open, setOpen] = useState(false);
+  const [delay, setDelay] = useState(false);
+  const [start, setStart] = useState(false);
+
+  const handleClick = () => {
+    setStart(true);
+    setOpen((prev) => !prev);
+    handleDelay(!open);
+  };
+
+  const handleDelay = (open: Boolean) => {
+    setTimeout(() => {
+      if (!open) {
+        setDelay(true);
+      } else {
+        setDelay(false);
+      }
+    }, 200);
+  };
+
+  return (
+    <div className={s.container}>
+      <img src={hamburger} alt="hamburger" onClick={handleClick} />
+      <div
+        className={`${s.bar} ${open ? s.openBar : s.closeBar}`}
+        style={delay ? { display: "none" } : start ? {} : { display: "none" }}
+      >
+        <div className={s.items}>
+          {items.map((item, index) => (
+            <NavItem key={`${item}_${index}`} title={item} select={select} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
