0ddd68143f2323f1790e6ae25803dee72ebc90eb
Fedex159 <fede.gallina94@gmail.com>
2021-12-05 18:10:13 -0300

    add description spanish

--- src/assets/jsons/projects.ts
+++ src/assets/jsons/projects.ts
@@ -4,7 +4,8 @@ import gifFinder from "../imgs/gif-finder.png";
 
 export interface Project {
   title: string;
-  description: string;
+  description?: string;
+  descriptionEs?: string;
   img: string;
   repo: string;
   prod: string;
@@ -15,6 +16,8 @@ export const projects: Project[] = [
     title: "Web Service",
     description:
       "E-commerce of sales and acquisition of services. Includes cart, payment methods, user panel, chat and many other features.",
+    descriptionEs:
+      "E-commerce de ventas y adquisición de servicios. Incluye carrito, métodos de pago, panel de usuario, chat y muchas otras funciones.",
     img: webService,
     repo: "https://github.com/Fedex159/pf-web-service",
     prod: "https://pf-web-service.vercel.app/",
@@ -23,6 +26,8 @@ export const projects: Project[] = [
     title: "Videogames",
     description:
       "Video games app. Some of the main functions are: video game search, filtering, creation of new video games, paging.",
+    descriptionEs:
+      "App de videojuegos. Algunas de las funciones principales son: búsqueda de videojuegos, filtrado, creación de nuevos videojuegos, paginación. ",
     img: videoGames,
     repo: "https://github.com/Fedex159/pi-videogames/",
     prod: "https://pi-videogames-kappa.vercel.app/",
@@ -31,6 +36,8 @@ export const projects: Project[] = [
     title: "Gif Finder",
     description:
       "Gifs app. Some of the main functions are: GIF search, color selector, add to favorites, carousel.",
+    descriptionEs:
+      "App de Gifs. Algunas de las funciones principales son: búsqueda de GIF, selector de color, agregar a favoritos, carrusel.",
     img: gifFinder,
     repo: "https://github.com/Fedex159/gif-finder",
     prod: "https://gif-finder-mu.vercel.app/",
